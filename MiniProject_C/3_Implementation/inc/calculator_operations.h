09f87b9eae80f89dd0362e47b2fc7ba0146fd518
99003158 <75420705+99003158@users.noreply.github.com>
2020-12-07 07:16:33 +0530

    Update calculator_operations.h

--- MiniProject_C/3_Implementation/inc/calculator_operations.h
+++ MiniProject_C/3_Implementation/inc/calculator_operations.h
@@ -32,6 +32,7 @@ int subtract(int operand1, int operand2);
 * @param[in] operand2
 * @return Result of operand1 * operand2
 */
+//int multiply(int operand1, int operand2);
 int multiply(int operand1, int operand2);
 
 
