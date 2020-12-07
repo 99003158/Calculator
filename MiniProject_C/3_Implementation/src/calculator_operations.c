01bd8fed543f41224e166c6e2a4d0fce0bebbe34
99003158 <75420705+99003158@users.noreply.github.com>
2020-12-07 03:51:02 +0000

    Update calculator_operations.c

--- MiniProject_C/3_Implementation/src/calculator_operations.c
+++ MiniProject_C/3_Implementation/src/calculator_operations.c
@@ -6,7 +6,7 @@ int add(int operand1, int operand2)
     return operand1 + operand2;
 }
 
-int subtract(int operand1, int operand2)
+int subtract(int operand1, int operand2)// subtract of a number
 {
     return operand1 - operand2;
 }
