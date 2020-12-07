4cb843df56068f25e03c7c51b794e6d5eea13127
99003158 <75420705+99003158@users.noreply.github.com>
2020-12-07 03:46:16 +0000

    Update calculator_operations.c

--- MiniProject_C/3_Implementation/src/calculator_operations.c
+++ MiniProject_C/3_Implementation/src/calculator_operations.c
@@ -11,7 +11,9 @@ int subtract(int operand1, int operand2)
     return operand1 - operand2;
 }
 
+//int multiply(int operand1, int operand2)
 int multiply(int operand1, int operand2)
+
 {
     return operand1 * operand2;
 }
